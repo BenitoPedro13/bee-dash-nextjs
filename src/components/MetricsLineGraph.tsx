7c5bc26f568a499613aafb6d7b18c2b7e24eb2e8
Benito Pedro <benitopedro21@outlook.com>
2024-08-14 07:23:07 -0300

    🐛 FIX:

--- src/components/MetricsLineGraph.tsx
+++ src/components/MetricsLineGraph.tsx
@@ -41,17 +41,15 @@ const MetricsGraph = ({ data }: MetricsGraphProps) => {
       setHeading(heading);
       setMetric(computeMetric(data));
 
-      if (windowIsUndefined === false) {
-        if (
-          !windowIsUndefined &&
-          window.innerWidth < 425 &&
-          heading === "Impressões (Views)"
-        ) {
-          setHeading("Impressões");
-        }
+      if (
+        !windowIsUndefined &&
+        window.innerWidth < 425 &&
+        heading === "Impressões (Views)"
+      ) {
+        setHeading("Impressões");
       }
     }
-  }, [typeOfGraph, data, window]);
+  }, [typeOfGraph, data]);
 
   return (
     <div
